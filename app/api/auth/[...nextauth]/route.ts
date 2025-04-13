3070b893c4e08c5d1733e36e1b6df6986945a13f
irawanenrico <irawanenrico@gmail.com>
2025-04-13 20:46:22 +0700

    fixing nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,2 @@
-import NextAuth from 'next-auth';
-
-import { authOptions } from '@/lib/authOptions';
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST, handler };
+import { GET, POST } from '@/lib/auth';
+export { GET, POST };
